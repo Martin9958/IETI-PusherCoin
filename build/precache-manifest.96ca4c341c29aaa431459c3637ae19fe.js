cc87d1958f8e02eb0722cf616620f11e52e42dbf
Martin Cantor <andres.cantor-u@mail.escuelaing.edu.co>
2019-02-12 12:15:17 -0500

    Inicio Lab03

---
+++ build/precache-manifest.96ca4c341c29aaa431459c3637ae19fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1920d1fa1b6bb237f4d",
+    "url": "/static/css/main.4d007f24.chunk.css"
+  },
+  {
+    "revision": "f1920d1fa1b6bb237f4d",
+    "url": "/static/js/main.f1920d1f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8e574e1b745f6653547e61b5472df09e",
+    "url": "/index.html"
+  }
+];
